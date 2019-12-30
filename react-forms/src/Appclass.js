def5f45f984ef9f8ed17d7b32bffed696f6a51f7
Carlos Fabio <cfabioramos@gmail.com>
2019-12-30 10:32:09 -0200

    react forms e react hook form

---
+++ react-forms/src/Appclass.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react"
+
+class Appclass extends Component {
+    constructor() {
+        super()
+        this.state = {}
+    }
+    
+    render() {
+        return (
+            <div>
+                Hello world...
+            </div>
+        )
+    }
+}
+
+export default Appclass
