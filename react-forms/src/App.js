07c4ba1358de10bb8fc83ade719a26089b6f1c05
Carlos Fabio <cfabioramos@gmail.com>
2020-01-02 11:17:30 -0200

    Forms

--- react-forms/src/App.js
+++ react-forms/src/App.js
@@ -1,10 +1,67 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [form, setForm] = useState({firstName: "", 
+        lastName: "", informed: true, details: "", 
+        gender: "Feminino", cor: "red"})
+
+  const handleChange = (event) => {
+    console.log(form)
+    const {name, value, type, checked} = event.target
+    type === "checkbox" ? setForm( {...form, ...{[name]: checked} }) : setForm( {...form, ...{[name]: value} })
+  }
+
   return (
     <div>
-        Hello world...
+        <input type="text" 
+          name="firstName" 
+          placeholder="Primeiro Nome" 
+          value={form.firstName}
+          onChange={handleChange} />
+        <br />
+        <input type="text" 
+            name="lastName" 
+            placeholder="Último Nome" 
+            value={form.lastName}
+            onChange={handleChange} />
+        <br />
+        <input 
+          type="checkbox"
+          name="informed"
+          checked={form.informed} 
+          onChange={handleChange}/>
+        <br />
+        <textarea name="details" value={form.details} onChange={handleChange}/>
+        <br />
+
+        <label>
+          <input 
+            type="radio"
+            name="gender"
+            value="Masculino"
+            onChange={handleChange}
+            checked={form.gender === "Masculino"}/> Masculino
+        </label>
+        <label>
+          <input 
+            type="radio"
+            name="gender"
+            value="Feminino"
+            onChange={handleChange}
+            checked={form.gender === "Feminino"}/> Feminino
+        </label>
+
+        <label>Cor: </label>
+        <select name="cor" value={form.cor} onChange={handleChange}>
+          <option value="">Selecione...</option>
+          <option value="red">Red</option>
+          <option value="blue">Blue</option>
+          <option value="green">Green</option>
+        </select>
+
+        <h3>{form.firstName} {form.lastName} {form.cor} {form.informed ? "Informado" : "Desinformado"} {form.details}</h3>
     </div>
   );
 }
--- react-forms/src/Appclass.js
+++ react-forms/src/Appclass.js
@@ -1,15 +1,39 @@
 import React, {Component} from "react"
 
 class Appclass extends Component {
+
     constructor() {
         super()
-        this.state = {}
+        this.state = {
+            firstName: "Carlos",
+            lastName: ""
+        }
+
+        this.handleChange = this.handleChange.bind(this)
     }
-    
+
+    handleChange(event) {
+        const {name, value} = event.target
+        this.setState({[name]: value})
+    }
+
     render() {
         return (
             <div>
-                Hello world...
+                <input type="text" 
+                    name="firstName" 
+                    placeholder="Primeiro Nome" 
+                    value={this.state.firstName}
+                    onChange={this.handleChange} />
+                <br />
+                <input type="text" 
+                    name="lastName" 
+                    placeholder="Último Nome" 
+                    value={this.state.lastName}
+                    onChange={this.handleChange} />
+                <br />
+                <h3>{this.state.firstName} {this.state.lastName}</h3>
+
             </div>
         )
     }
