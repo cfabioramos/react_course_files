003c2e1f620292b1e76a48194fd54b75041a7615
Carlos Fabio <cfabioramos@gmail.com>
2020-01-05 16:49:38 -0200

    Hook form

--- react-hook-form-app/src/App.js
+++ react-hook-form-app/src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useForm } from 'react-hook-form'
 
 function App() {
+
+  const { register, handleSubmit, errors } = useForm()
+  const submitForm = (data) => console.log (data)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit(submitForm)}>
+        <label>Primeiro Nome: </label>
+        <input type="text" name="firstName" ref={register({required: true, minLength: 3})}/>
+        { errors.firstName && errors.firstName.type === "required" && <span>Campo Obrigatório</span> } 
+        { errors.firstName && errors.firstName.type === "minLength" && <span>Mínimo de 3 caracteres</span> } 
+        <br/>
+        <label>Segundo Nome: </label>
+        <input type="text" name="lastName" ref={register}/><br/>
+        <select name="cor" ref={register}>
+          <option value="">Selecione</option>
+          <option value="p">Preto</option>
+          <option value="b">Branco</option>
+          <option value="a">Amarelo</option>
+        </select><br />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
